5beaeccb6ae190d436847cf1a01b582870480fa3
romainwidmer <romwidmer@gmail.com>
2018-10-17 14:02:58 +0200

    formatting dates with moment js

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './project.css'
+import moment from 'moment'
 
 const ProjectSummary = ({project}) => {
   return(
     <div className="card z-depth-0 project-summary">
       <span className="card-title">{project.title}</span>
-      <p>Posted by {project.authorLastName}</p>
-      <p className="grey-text darken-3">{project.date}</p>
+      <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
+      <p className="grey-text darken-3">{moment(project.createdAt.toDate()).format('LL')}</p>
     </div>
   )
 }
